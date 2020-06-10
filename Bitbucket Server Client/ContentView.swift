df987e9cfef846e026d990a37b3135830b1ba06c
Ian Applebaum <Ian.tyler@electricbluefish.com>
2020-06-10 19:51:01 -0400

    Initial Commit

---
+++ Bitbucket Server Client/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Bitbucket Server Client
+//
+//  Created by Ian Applebaum on 6/10/20.
+//  Copyright © 2020 Ian Applebaum. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
